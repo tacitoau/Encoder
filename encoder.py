a5d4b1551e32f87895e148b768bd14c15d9b66ac
Tácito Augusto Farias Júnior <81604617+tacitoau@users.noreply.github.com>
2021-09-12 09:16:39 -0300

    Initial commit

---
+++ encoder.py
@@ -0,0 +1,16 @@
+import sys 
+import urllib  
+ 
+input = '' 
+for line in sys.stdin: 
+input += line  
+ 
+print input,  
+ 
+for line in sys.stdin: 
+input += str(sys.stdout.write(line))  
+ 
+Input = sys.argv[1] 
+print sys.argv  
+ 
+print urllib.quote(input) 
